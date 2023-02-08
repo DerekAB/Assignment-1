897d89fadded24aa5169b5aa46b96966a4fdeb3e
DerekAB <123401143+DerekAB@users.noreply.github.com>
2023-02-08 02:45:43 +0000

    more additions

--- Dbaker4876-Assignment1.py
+++ Dbaker4876-Assignment1.py
@@ -7,8 +7,42 @@
 #This program will help Arnold take orders from customers and calculate the cost of the order
 #The program will ask for what they want to order, ask them to confirm their order, and then print them a reciept 
 
+
+
 din1 = "Austin's pemeal bacon"
 din2 = "Aaron's poutine"
+dinner = " "
+price = ""
+quantity = ""
+totalPrice = ""
+disPrice = ""
+tax = ""
+grandTotal = ''
+
+def formatBill(data, headers, size) :
+    lines = ""
+    line = ""
+    i = 1
+    for header in headers :
+        line += header + "|"
+        while len(line) < size * i :
+            line += " "
+        i += 1
+    lines = line + "\n"
+    lineLength = len(line)
+    for x in range(1, lineLength):
+        lines += "_"
+    lines += "\n"
+    for row in data:
+        line = ""
+        i = 1
+        for element in row:
+            line += element
+            while len(line) < size * i :
+                line += " "
+            i += 1
+        lines += line + "\n"
+    return lines
 
 def getDinnerOrder():
     dinner = float(input("Which dinner would you like? \n[1 - " + din1 + "] \n[2 - " + din2 + "] \n"))
@@ -22,6 +56,7 @@ def getDinnerOrder():
     quantity = int(input("How many would you like?: "))  
       
     totalPrice = price * quantity
+    tax = totalPrice * 0.13
     
     if totalPrice == range(100 - 500):
         disPrice = totalPrice * 0.1
@@ -47,10 +82,7 @@ def getDinnerOrder():
     if confirm == "y":
         return False
     
-
-
-
-answer = input("WELCOME TO ARNOLD'S AMAZING EATS!! ARE YOU HERE TO ORDER FOOD OR WHAT? [Y/N]: ").lower()
+answer = input("WELCOME TO ARNOLD'S AMAZING EATS!! ARE YOU HERE TO ORDER FOOD OR WHAT? [Y/N]: ").strip().lower()
 
 if answer == "n":
     exit() 
@@ -79,5 +111,12 @@ while getDinnerOrder():
     if True:
         print("Please reenter your order.")
         
-student = input("Are you a student? [Y/N]: ")
+studentDiscount = (grandTotal * 0.1) 
+studentDif = grandTotal + (grandTotal + studentDiscount)
 
+headers = ["Order", "Item Amount", "Item Price", "Total"]
+
+student = input("Are you a student? [Y/N]: ").strip().lower()
+if student == "y":
+    data = [dinner, str(quantity), '$' + str(price), '$' + str(grandTotal)], ['10% Student Savings', '', '', ]
+    print(formatBill(headers, data))
